3eff85b5c018f789e70eabc78c4c9f6bc5051b97
armaninformatics <ammangdeveloper@gmail.com>
2024-04-14 06:53:47 +0800

    belajar express js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ exports.getAllUser = (req, res) => {
     fetch('https://reqres.in/api/users?page=2')
         .then((response) => response.json())
         .then((data) => {
+            // res.json(data)
             res.render('index', {users: data})
         })
 };
